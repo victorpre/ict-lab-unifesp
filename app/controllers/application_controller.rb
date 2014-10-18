6f94fba5b0aceec6e3b2b0128c6c666e2766451e
Victor Presumido <victor@Victors-MacBook-Pro.local>
2014-10-18 04:44:08 -0300

    Implements simple login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  private
+
+  def index
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
