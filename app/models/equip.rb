8082b087982b2df7e0b827e16a09c832202a4356
Victor Presumido <victor@Victors-MacBook-Pro.local>
2014-11-01 14:31:28 -0200

    Equips registration validation

--- app/models/equip.rb
+++ app/models/equip.rb
@@ -1,2 +1,6 @@
 class Equip < ActiveRecord::Base
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :patrimony_id, presence: true, :uniqueness => {:case_sensitive => false}
+  validates :cost, presence: true
 end
