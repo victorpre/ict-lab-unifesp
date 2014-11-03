74b19f59508b6397d8f46b06d7bb90088a06ce03
victorpre <victor.presumido@gmail.com>
2014-11-03 11:30:54 -0200

    Seed db with 10 fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+1.upto(10) do |i|
+  User.create(:name => "User#{i}",
+              :email => "user#{i}@gmail.com",
+              :password => "123123123",
+              :institution => "Unifesp",
+              :locked => false
+              )
+end
