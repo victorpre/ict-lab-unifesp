63b584690fb66c1be45d64da48bcce36c44139f1
Victor Presumido <victor@Victors-MacBook-Pro.local>
2014-11-19 19:12:40 -0200

    Completes User registratrion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  self.inheritance_column = nil
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
