2c5fb7ce5315d58266ed74c7bd7ac1de994a6b4b
victorpre <victor.presumido@gmail.com>
2014-11-25 13:44:39 -0200

    git add app/controllers/schedules_controller.rbgit add app/controllers/schedules_controller.rb

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -22,16 +22,13 @@ class SchedulesController < ApplicationController
   def create
     user = User.find(schedule_params["user_id"])
     equip = Equip.find(schedule_params["equip_id"])
-    binding.pry
     # Tempo do agendamento em minutos
     schedule_length = (schedule_params["end_date"].to_time - schedule_params["start_date"].to_time)/60
-
     # Verifica se horario esta disponivel
     if (!equip.scheduled?(schedule_params["start_date"].to_date,schedule_params["start_date"].to_time..schedule_params["end_date"].to_time))
       # Verifica se duração é menor que 2 hrs, se o usuário não tem mais de 2 horas agendadas no dia e se o agendamento atual mais os outros agendamento não ultrapassam 2 hrs
       if (schedule_length < 120 && !user.two_hours_in_day?(schedule_params["start_date"].to_date) && (schedule_length + user.minutes_in_day(schedule_params["start_date"].to_date) < 120))
         @schedule = Schedule.new(schedule_params)
-        binding.pry
         if @schedule.save
           render json: @schedule.id
         else
