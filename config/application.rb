dd69a72953d242f1e12fb036099f7f9cd900baaa
victorpre <victor.presumido@gmail.com>
2014-11-25 13:02:25 -0200

    Configures timezone

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module LabIctUnifesp
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.time_zone = 'Brasilia'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
