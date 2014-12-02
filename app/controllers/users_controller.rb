2bd812c559e26643d941fb073b829dcefa28cf38
Renan <renan.augusto18@hotmail.com>
2014-12-02 20:40:27 -0200

    Changes in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where("id != ?" , current_user.id).order('name ASC')
     respond_with(@users)
   end
 
